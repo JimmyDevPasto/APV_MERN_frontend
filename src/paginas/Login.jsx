e1bf373c974e64f2f8d86428bc44ddf4e1be4540
Jimmy <jimmydeveloperpasto@gmail.com>
2022-07-25 15:56:43 -0500

    ultimo

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -56,7 +56,7 @@ const Login = () => {
       
           <div className="">
             <h1 className='text-indigo-600 font-black text-6xl'>
-              Inicia Sesión y Administra tus  {""}<span className='text-black'>Pacientes Jimmy Rosero</span>
+              Inicia Sesión y Administra tus  {""}<span className='text-black'>Pacientes Jimmy Rosero.</span>
             </h1>
           </div>
           <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
